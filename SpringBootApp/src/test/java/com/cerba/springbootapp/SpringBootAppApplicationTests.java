89ae1776591723f4dfb5e31606ec87ccebee8ad4
Nielsstrychi <niels_strychi@hotmail.com>
2024-04-30 11:51:34 +0200

    Docker added & db

--- SpringBootApp/src/test/java/com/cerba/springbootapp/SpringBootAppApplicationTests.java
+++ SpringBootApp/src/test/java/com/cerba/springbootapp/SpringBootAppApplicationTests.java
@@ -1,9 +1,15 @@
 package com.cerba.springbootapp;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
 
 @SpringBootTest
+@ActiveProfiles("test")
+//@AutoConfigureEmbeddedDatabase(type = AutoConfigureEmbeddedDatabase.DatabaseType.POSTGRES, provider = ZONKY)
 class SpringBootAppApplicationTests {
 
     @Test
