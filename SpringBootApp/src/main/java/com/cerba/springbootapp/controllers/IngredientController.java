a4cc5fc3b8a25882387b5dd13511dc4a8e17fcde
Nielsstrychi <niels_strychi@hotmail.com>
2024-04-30 12:35:11 +0200

    setup controller methods

--- SpringBootApp/src/main/java/com/cerba/springbootapp/controllers/IngredientController.java
+++ SpringBootApp/src/main/java/com/cerba/springbootapp/controllers/IngredientController.java
@@ -1,4 +1,45 @@
 package com.cerba.springbootapp.controllers;
 
+import com.cerba.springbootapp.datatransferobjects.IngredientDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/ingredients")
 public class IngredientController {
-}
+
+    @GetMapping
+    public Flux<Page<IngredientDTO>> getAllIngredients(@RequestParam(defaultValue = "0", required = false) int page,
+                                                       @RequestParam(defaultValue = "10", required = false) int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<IngredientDTO> getIngredientById(@PathVariable UUID id) {
+            return null;
+    }
+
+    @PostMapping
+    public Mono<IngredientDTO> createIngredient(@RequestBody IngredientDTO ingredient) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Mono<IngredientDTO> updateIngredient(@PathVariable UUID id, @RequestBody IngredientDTO updatedIngredient) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable UUID id) {
+        return null;
+    }
+
+    }
--- SpringBootApp/src/main/java/com/cerba/springbootapp/controllers/RecipeController.java
+++ SpringBootApp/src/main/java/com/cerba/springbootapp/controllers/RecipeController.java
@@ -1,4 +1,44 @@
 package com.cerba.springbootapp.controllers;
 
+import com.cerba.springbootapp.datatransferobjects.RecipeDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/recipes")
 public class RecipeController {
+
+    @GetMapping
+    public Flux<Page<RecipeDTO>> getAllRecipes(@RequestParam(defaultValue = "0", required = false) int page,
+                                               @RequestParam(defaultValue = "10", required = false) int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<RecipeDTO> getRecipeById(@PathVariable UUID id) {
+        return null;
+    }
+
+    @PostMapping
+    public Mono<RecipeDTO> createRecipe(@RequestBody RecipeDTO Recipe) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Mono<RecipeDTO> updateRecipe(@PathVariable UUID id, @RequestBody RecipeDTO updatedRecipe) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecipe(@PathVariable UUID id) {
+        return null;
+    }
 }
